daf227ae42c1414979ca4fa70c432ca03945ee59
Camilo Pinzon <pinzonac@gmail.com>
2024-04-04 14:15:42 -0500

    Add styles

--- public/script.js
+++ public/script.js
@@ -1,9 +1,3 @@
-const handleClick = (e) => {
-    const checkbox = e.target;
-    let checked = checkbox.checked;
-    checkbox.setAttribute("checked", checked);
-}
-
 const handleDelete = (e) => {
     const idToDelete = `${e.target.id.split("-")[1]}`;
     //make a fetch request to delete the item with the idToDelete
