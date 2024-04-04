d034e1f825abbea7b017ff3271d6fcb623e3f0ef
Camilo Pinzon <pinzonac@gmail.com>
2024-04-04 12:50:29 -0500

    Remove selected todo from todos array

--- public/script.js
+++ public/script.js
@@ -3,3 +3,16 @@ const handleClick = (e) => {
     let checked = checkbox.checked;
     checkbox.setAttribute("checked", checked);
 }
+
+const handleDelete = (e) => {
+    const idToDelete = `${e.target.id.split("-")[1]}`;
+    //make a fetch request to delete the item with the idToDelete
+    fetch(`/delete/${idToDelete}`, { method: "DELETE" })
+        .then(response => {
+            if (response.ok) {
+                window.location.reload();
+            } else {
+                throw new Error("Failed to delete item.");
+            }
+    })
+}
